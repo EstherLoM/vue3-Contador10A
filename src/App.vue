755c80709a702881fb729b6e528278a4c7067a4e
EstherLoM <a3518110299@alumno.uttehuacan.edu.mx>
2022-09-13 01:04:59 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script>
+
+export default {
+	data: () => ({
+		contador:500,
+		ventana:false,
+	}),
+	methods: {
+	 incrementar(){
+	 this.contador++;
+	 },
+	 disminuir(){
+	 this.contador--;
+	 },
+	 resetear(){
+	 this.contador=10;
+	 },
+	}
+};
+</script>
+
+<template> 
+    <div class="container">
+   
+		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+	    <small>
+		<h1>Hola desde VUE.js 3</h1>
+		</small>
+		<h2>Contador {{ contador }}</h2>
+
+		<button @click="incrementar()"> Sumar uno</button>
+		<button @click="disminuir()">Restar uno</button>
+		<button @click="resetear()">Reiniciar</button><br>
+
+    </div>
+
+	<button @click.prevente="ventana=true"> Abrir</button>
+
+   <div v-bind="ventana" v-if="ventana">
+
+   <b>Nombre:</b><i>Lopez Muñoz Esther</i><br>
+   <b>Materia:</b><i>Desarrollo Movil Integral</i><br>
+   <b>Profesor:</b><i>Hector Bernardo Ortega Gines</i><br>
+   <b><i>10mo A - DGS</i></b><br>
+   <button @click.prevente="ventana=false"> Cerrar</button>
+   </div>
+</template>
+
+<style scoped>
+
+.container{
+	text-align: center;
+	}
+	
+	h1{
+	color: Blue;
+	font-weight:bolder;
+	}
+
+	h2{
+	font-size:2.5rem;
+	}
+	
+	
+	button{
+	cursor: pointer;
+	font-size: 1.5rem;
+	margin: 0px 8px;
+	}
+	
+	small{
+	color: #666;
+	}
+	
+
+</style>
---
+++ src/components/Popup.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class= "popup">
+		<div class= "popup-inner">
+			<button>Open</button>
+			<s lot/>
+			<button class="popup-close">Cerrar</button>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.popu{
+position: fixed;
+top:0;
+botton:0;
+left:0;
+right:0;
+z-index:99;
+background-color; rgba(0,0,0,0.2);
+display :flex;
+align-items: center;
+justtify-content: center;
+}
+
+.popup-inner{
+background:#fff;
+padding 32px;
+}
+</style>
